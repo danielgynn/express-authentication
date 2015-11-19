61342c20ceb71c56d0fb9741567ce871e48cbf17
danielgynn <danielgynn@hotmail.co.uk>
2015-11-19 19:04:34 +0000

    Set user model schema

---
+++ models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+  local: {
+    name: String,
+    email: String,
+    password: String,
+  },
+});
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
