14949cc4726c786d1596651dc1c590ba39205ae9
danielgynn <danielgynn@hotmail.co.uk>
2015-11-19 18:32:28 +0000

    Add basic routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var passpor = require('passport');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login.ejs', { title: 'Express' });
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup.ejs', { title: 'Express' });
+});
+
+router.get('/profile', isLoggedIn, function(req, res) {
+  res.render('profile.ejs', { user: req.user });
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
