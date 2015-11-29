d2fef494dd7ac30299fab2ea18a2a13f14fbaae8
danielgynn <danielgynn@hotmail.co.uk>
2015-11-29 09:50:48 +0000

    add auth file

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+  facebookAuth: {
+    clientID: '896605267054808',
+    clientSecret: '28f6a7d2842d5f9ba19e449131694902',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  },
+  twitterAuth: {
+    consumerKey: 'sOM9aZpoEyNAXKqvEUHd2O7cN',
+    consumerSecret: '6I1x0PgH41AxuqtAKcfRqYtWDMKNv0TBaaoegW3RP3o1ks7NPk',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+  },
+  googleAuth: {
+    clientID: '262575533252-sviar9bm39g5c4dnlb6erkpvjme4hit2.apps.googleusercontent.com',
+    clientSecret: '1K1fRgOmy1WJK4_Z8ycGRyuz',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  },
+};
