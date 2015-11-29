53db7ed3c543bc94d7abb8ca9f8f444970e6bb6b
danielgynn <danielgynn@hotmail.co.uk>
2015-11-29 14:53:41 +0000

    Add passport configurations

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,9 @@
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/user');
+var configAuth = require('./auth');
 
 module.exports = function(passport) {
 
@@ -55,5 +59,91 @@ module.exports = function(passport) {
       return done(null, user);
     });
   }));
+
+  passport.use(new FacebookStrategy({
+    clientID: configAuth.facebookAuth.clientID,
+    clientSecret: configAuth.facebookAuth.clientSecret,
+    callbackURL: configAuth.facebookAuth.callbackURL,
+    profileFields: ['id', 'email', 'first_name', 'last_name'],
+  },
+  function(token, refreshToken, profile, done) {
+    process.nextTick(function() {
+      User.findOne({ 'facebook.id': profile.id }, function(err, user) {
+        if (err)
+          return done(err);
+        if (user) {
+          return done(null, user);
+        } else {
+          var newUser = new User();
+          newUser.facebook.id = profile.id;
+          newUser.facebook.token = token;
+          newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+          newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
+
+          newUser.save(function(err) {
+            if (err)
+              throw err;
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+
+  passport.use(new TwitterStrategy({
+    consumerKey: configAuth.twitterAuth.consumerKey,
+    consumerSecret: configAuth.twitterAuth.consumerSecret,
+    callbackURL: configAuth.twitterAuth.callbackURL,
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function() {
+      User.findOne({ 'twitter.id': profile.id }, function(err, user) {
+        if (err)
+          return done(err);
+        if (user) {
+          return done(null, user);
+        } else {
+          var newUser = new User();
+          newUser.twitter.id          = profile.id;
+          newUser.twitter.token       = token;
+          newUser.twitter.username    = profile.username;
+          newUser.twitter.displayName = profile.displayName;
+          newUser.save(function(err) {
+            if (err)
+             throw err;
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+
+  passport.use(new GoogleStrategy({
+    clientID: configAuth.googleAuth.clientID,
+    clientSecret: configAuth.googleAuth.clientSecret,
+    callbackURL: configAuth.googleAuth.callbackURL,
+  },
+    function(token, refreshToken, profile, done) {
+      process.nextTick(function() {
+        User.findOne({ 'google.id': profile.id }, function(err, user) {
+          if (err)
+            return done(err);
+          if (user) {
+            return done(null, user);
+          } else {
+            var newUser = new User();
+            newUser.google.id = profile.id;
+            newUser.google.token = token;
+            newUser.google.name = profile.displayName;
+            newUser.google.email = profile.emails[0].value;
+            newUser.save(function(err) {
+              if (err)
+                throw err;
+              return done(null, newUser);
+            });
+          }
+        });
+      });
+    }));
 
 };
