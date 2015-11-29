8383af990a4fd1a68782216080b9ee9a98538bec
danielgynn <danielgynn@hotmail.co.uk>
2015-11-29 15:08:00 +0000

    Add new routes for passport

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,27 @@ router.post('/login', passport.authenticate('local-login', {
   failureFlash: true,
 }));
 
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback', passport.authenticate('twitter', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
