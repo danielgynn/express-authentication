9d450502d1ee7be6f5bf55be65495ff13d882658
m.jankovsky <m.jankovsky@gmc.net>
2016-07-26 22:50:57 +0200

    deprecated express-session init options

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: 'shhsecret' }));
+app.use(session({ secret: 'shhsecret', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
